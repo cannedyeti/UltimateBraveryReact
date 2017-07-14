a2aad41a8a0469ae676233af2c60f5d0e137f93f
cannedyeti <cpotebnya@gmail.com>
2017-07-13 17:21:07 -0700

    added photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 const React = require('react');
+const Dooder = require('./../img/doodiegrill.png');
+var NavLink = require('react-router-dom').NavLink;
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <h2>All for you Doodie</h2>
+        <h2>Congrats to Dooder and his new grill!</h2>
+        <img className="dooder" src={Dooder} alt="dooder's hot bitch" />
+        <h4>Now let's go get <NavLink  to='/ultimate-bravery'>brave</NavLink> you bitches.</h4>
       </div>
     )
   }
